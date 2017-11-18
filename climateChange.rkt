c5dafdc14ae9a84a6194c5ab710b7c35614d5a98
klyngdoh <lyngdoh.k@husky.neu.edu>
2017-11-18 15:09:50 -0800

    Added Mouse Events

--- climateChange.rkt
+++ climateChange.rkt
@@ -5,8 +5,29 @@
 (require "extras.rkt")
 (require "pictures.rkt")
 (require 2htdp/universe)
-(require "MouseEvents.rkt")
 
+(define BUTTON1H 50)
+(define BUTTON1W 50)
+(define BUTTON1X 100)
+(define BUTTON1Y 100)
+
+(define BUTTON2H 50)
+(define BUTTON2W 50)
+(define BUTTON2X 300)
+(define BUTTON2Y 100)
+
+(define BUTTON3H 50)
+(define BUTTON3W 50)
+(define BUTTON3X 500)
+(define BUTTON3Y 100)
+
+(define CAR-INIT-X 30)
+(define CAR-INIT-VX 5)
+
+(define FACT-INIT-X 25)
+
+(define MOUSE-UP "button-up")
+(define MOUSE-DOWN "button-down")
 
 (define BOUNDRY-CAR-LEFT 15)
 (define BOUNDRY-CAR-RIGHT 35)
@@ -325,3 +346,82 @@
 ;; CONSTRUCTOR TEMPLATE:
 (define-struct tree (x))
 
+;; world-after-mouse-event: World MouseEvent Int Int -> World
+;; GIVEN: A world, a mouse event and its coordinates
+;; RETURNS: A world after the given mouse event
+;; DESIGN STRATEGY: Divide into cases
+
+(define (world-after-mouse-event w mev x y)
+  (cond
+    [(and (< (- BUTTON1X (/ BUTTON1W 2)) x (+ BUTTON1X (/ BUTTON1W 2)))
+          (< (- BUTTON1Y (/ BUTTON1H 2)) y (+ BUTTON1Y (/ BUTTON1H 2)))
+          (mouse=? mev MOUSE-UP))
+     (world-after-b1-press w)]
+    [(and (< (- BUTTON2X (/ BUTTON2W 2)) x (+ BUTTON2X (/ BUTTON2W 2)))
+          (< (- BUTTON2Y (/ BUTTON2H 2)) y (+ BUTTON2Y (/ BUTTON2H 2)))
+          (mouse=? mev MOUSE-UP))
+     (world-after-b2-press w)]
+    [(and (< (- BUTTON3X (/ BUTTON3W 2)) x (+ BUTTON3X (/ BUTTON3W 2)))
+          (< (- BUTTON3Y (/ BUTTON3H 2)) y (+ BUTTON3Y (/ BUTTON3H 2)))
+          (mouse=? mev MOUSE-UP))
+     (world-after-b3-press w)]
+    [else w]))
+
+;; world-after-b1-press: World -> World
+;; GIVEN: A world
+;; RETURNS: A world after button1 is pressed in the given world
+;; DESIGN STRATEGY: Use Constructor template on World
+
+(define (world-after-b1-press w)
+  (make-world (add-car (world-cars w))
+              (world-trees w)
+              (world-factories w)
+              (world-state w)))
+
+;; world-after-b2-press: World -> World
+;; GIVEN: A world
+;; RETURNS: A world after button2 is pressed in the given world
+;; DESIGN STRATEGY: Use Constructor template on World
+
+(define (world-after-b2-press w)
+  (make-world (world-cars w)
+              (remove-tree (world-trees w))
+              (world-factories w)
+              (world-state w)))
+
+;; world-after-b3-press: World -> World
+;; GIVEN: A world
+;; RETURNS: A world after button3 is pressed in the given world
+;; DESIGN STRATEGY: Use Constructor template on World
+
+(define (world-after-b3-press w)
+  (make-world (world-cars w)
+              (world-trees w)
+              (add-factory (world-factories w))
+              (world-state w)))
+
+;; add-car: CarList -> CarList
+;; GIVEN: A list of cars
+;; RETURNS: A similar list of cars, but with one more car
+;; DESIGN STRATEGY: Use simpler function
+
+(define (add-car cl)
+  (append cl (list (make-car (random 50 450) CAR-INIT-VX))))
+
+;; remove-tree: TreeList -> TreeList
+;; GIVEN: A list of trees
+;; RETURNS: A similar list of trees but with one tree removed
+;; DESIGN STRATEGY: Divide into cases
+
+(define (remove-tree tl)
+  (cond
+    [(or (empty? tl) (empty? (rest tl))) empty]
+    [else (cons (first tl) (remove-tree (rest tl)))]))
+
+;; add-factory: FactoryList -> FactoryList
+;; GIVEN: A list of factories
+;; RETURNS: A similar list of factories but with a new one added
+;; DESIGN STRATEGY: Use simpler functions
+
+(define (add-factory fl)
+  (append fl (list (make-factory (random 50 450)))))
