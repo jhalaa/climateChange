5ab3b64c3ceb3b25e2ba5bc6dfa207c3347c7b26
klyngdoh <lyngdoh.k@husky.neu.edu>
2017-11-18 15:00:27 -0800

    Required file

--- MouseEvents.rkt
+++ MouseEvents.rkt
@@ -1,10 +1,11 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname world-after-mouse-event) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname MouseEvents) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 (require rackunit)
 (require "extras.rkt")
 (require 2htdp/image)
 (require 2htdp/universe)
+(require "climateChange.rkt")
 
 (provide
  world-after-mouse-event)
